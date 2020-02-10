52fd69b227433f7c8ec428b11ab87e5b84848fe3
Helana Maliakal <maliakaa@tcd.ie>
2020-02-10 14:27:31 +0000

    testing nltk

--- src/gutenbergtest.py
+++ src/gutenbergtest.py
@@ -1,5 +1,12 @@
 from gutenberg.acquire import load_etext
 from gutenberg.cleanup import strip_headers
+import nltk
 
 text = strip_headers(load_etext(2701)).strip()
-print(text)
+tokenised_sentences = nltk.sent_tokenize(text)
+for sentence in tokenised_sentences:
+    tokenised_words = nltk.word_tokenize(sentence)
+
+
+    tagged_words = nltk.pos_tag(tokenised_words)
+    
