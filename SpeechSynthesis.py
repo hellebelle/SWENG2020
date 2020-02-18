f1cfb8609a38a62a36bcb5ff6430d53f728df236
Mike Kelly <mkelly32@tcd.ie>
2020-02-18 07:20:57 +0000

    Added speech synthesis, with the aim to replace tts. Needs to be tested on windows, as Arch does not support a necessary library

---
+++ SpeechSynthesis.py
@@ -0,0 +1,28 @@
+
+
+# Creates an instance of a speech config with specified subscription key and service region.
+# Replace with your own subscription key and service region (e.g., "westus").
+speech_key, service_region = "78327e2366f64dbeab97595021f4319e", "westeurope"
+speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+
+# Creates a speech synthesizer using the default speaker as audio output.
+speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
+
+# Receives a text from console input.
+print("Type some text that you want to speak...")
+text = input()
+
+# Synthesizes the received text to speech.
+# The synthesized speech is expected to be heard on the speaker with this line executed.
+result = speech_synthesizer.speak_text_async(text).get()
+
+# Checks result.
+if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+    print("Speech synthesized to speaker for text [{}]".format(text))
+elif result.reason == speechsdk.ResultReason.Canceled:
+    cancellation_details = result.cancellation_details
+    print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+    if cancellation_details.reason == speechsdk.CancellationReason.Error:
+        if cancellation_details.error_details:
+            print("Error details: {}".format(cancellation_details.error_details))
+    print("Did you update the subscription info?")
