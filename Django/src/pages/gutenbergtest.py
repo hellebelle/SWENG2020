3c38a2fa010fc46b5aaed659be205372cb659f4e
Thomas Keyes <keyest@tcd.ie>
2020-03-19 12:35:00 +0000

    Update gutenbergtest.py
    Added function to determine syllable type

--- Django/src/pages/gutenbergtest.py
+++ Django/src/pages/gutenbergtest.py
@@ -8,6 +8,47 @@ from nltk.tokenize import SyllableTokenizer
 #nltk.download('punkt')
 #nltk.download('averaged_perceptron_tagger')
 
+
+def getSyllableType(syllable):
+
+    syllable = syllable.lower()
+    lastIndex = len(syllable) - 1
+
+    vowels = "aeiouy"
+    syll = False
+    count = 0
+
+    for i in range(0, lastIndex + 1):
+
+        if syllable[i] in vowels:
+            syll = True
+            count+=1
+
+    if not syll:
+        return "Error"
+
+    if syllable[lastIndex] == 'e':
+
+        if syllable[lastIndex - 1] == 'l':
+            return "-le"
+        if count >= 2 and not syllable[lastIndex - 1] in vowels: # At least two vowels (the last being 'e') with the second last letter being a consonant
+            return "Magic E"
+
+    if syllable[lastIndex] in vowels:
+        return "Open"
+
+    if syllable[lastIndex] == 'r':
+        if syllable[lastIndex - 1] in vowels:
+            return "R Controlled"
+
+    for i in range(0, lastIndex):
+
+        if syllable[i] in vowels:
+            if syllable[i+1] in vowels:
+                return "Vowel Team"
+
+    return "Closed"
+
 def createBookPages(text, charCount):
     
     pageText = ([text[i:i+charCount] for i in range(0, len(text), charCount)]) 
@@ -60,26 +101,34 @@ def getBookTextByNumber(bookID, strip):
 
 #findBookByAuthor('Melville, Hermann')
 
-bookText = getBookTextByNumber(2701, True)
+print(getSyllableType("test"))
+print(getSyllableType("be"))
+print(getSyllableType("came"))
+print(getSyllableType("far"))
+print(getSyllableType("team"))
+print(getSyllableType("cle"))
+print(getSyllableType("BRB"))
 
-#print(getBookPage(text, 3000, 1))
+# bookText = getBookTextByNumber(2701, True)
 
-pageText = getBookPage(bookText, 3000, 1)
+# #print(getBookPage(text, 3000, 1))
 
-print("Created Page 1")
+# pageText = getBookPage(bookText, 3000, 1)
 
-SSP = SyllableTokenizer()
+# print("Created Page 1")
 
-
-tokenised_sentences = nltk.sent_tokenize(pageText)
-for sentence in tokenised_sentences:
-    tokenised_words = nltk.word_tokenize(sentence)
+# SSP = SyllableTokenizer()
 
 
-    tagged_words = nltk.pos_tag(tokenised_words)
-    for word in tokenised_words:
-        tokenised_syllables = SSP.tokenize(word)
-        #print(tokenised_syllables)
+# tokenised_sentences = nltk.sent_tokenize(pageText)
+# for sentence in tokenised_sentences:
+#     tokenised_words = nltk.word_tokenize(sentence)
+
+
+#     tagged_words = nltk.pos_tag(tokenised_words)
+#     for word in tokenised_words:
+#         tokenised_syllables = SSP.tokenize(word)
+#         #print(tokenised_syllables)
     
 
 
