7cf818124b8c91e1e833a8dbbba5cb2311f4d815
Mike Kelly <mkelly32@tcd.ie>
2020-03-25 22:50:44 +0000

    Changed googleTTS to be a python function

--- Django/src/pages/googleTTS.py
+++ Django/src/pages/googleTTS.py
@@ -1,7 +1,6 @@
 from google_speech import Speech
 
-print("Please enter text to synthesize:")
-text = input()
-lang = "en"
-speech = Speech(text, lang)
-speech.play()
+def TTS(text):
+    lang = "en"
+    speech = Speech(text, lang)
+    speech.play()
