4e1d33cee23cc6627be2f3c98edc32721595cc08
Thomas Keyes <keyest@tcd.ie>
2020-04-13 16:03:37 +0100

    filteredText is now converted to one singular string for display

--- Dyslexia Friendly Books/pages/views.py
+++ Dyslexia Friendly Books/pages/views.py
@@ -145,5 +145,47 @@ def removeStopWords(text):
         if w not in stopWords: 
             filteredText.append(w) 
 
+    filteredText = " ".join(filteredText)
     return filteredText
 
+#returns the type of syllable, 
+#returns "Error" if the text contains no vowels and is therefore not a syllable   
+def getSyllableType(syllable):
+
+    syllable = syllable.lower()
+    lastIndex = len(syllable) - 1
+
+    vowels = "aeiouy"
+    syll = False
+    count = 0
+
+    for i in range(0, lastIndex + 1):
+
+        if syllable[i] in vowels:
+            syll = True
+            count += 1
+
+    if not syll:
+        return "Error"
+
+    if syllable[lastIndex] == 'e':
+
+        if syllable[lastIndex - 1] == 'l':
+            return "-le"
+        if count >= 2 and not syllable[lastIndex - 1] in vowels: # At least two vowels (the last being 'e') with the second last letter being a consonant
+            return "Magic E"
+
+    if syllable[lastIndex] in vowels:
+        return "Open"
+
+    if syllable[lastIndex] == 'r':
+        if syllable[lastIndex - 1] in vowels:
+            return "R Controlled"
+
+    for i in range(0, lastIndex):
+
+        if syllable[i] in vowels:
+            if syllable[i+1] in vowels:
+                return "Vowel Team"
+
+    return "Closed"
